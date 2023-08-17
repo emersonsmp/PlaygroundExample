7ba2eb6ea8f0e2ea18d03175057a0cd5a5cf3711
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2023-08-17 13:59:27 -0300

    exercise
    exercise

---
+++ Dio-Project.swiftpm/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let firstNAme: String = "Steve"
+var lastname: String? = "Jobs"
+
+print("\(firstNAme) \(surname ?? "Wozniak")")
+
+if let unwrappedLastName = lastName {
+    print("Nome completo desembrulhado: \(firstName) \(unwrappedLastName)")
+}
+
+
